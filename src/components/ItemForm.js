d842a3da1fbf70fe528930ebfa3d7c9b37204b21
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-11 15:34:57 +1100

    complete

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,28 +1,53 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ handleAddedItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleName(e) {
+    const nameValue = e.target.value;
+    setName(nameValue);
+  }
+
+  function handleCategorySelect(e) {
+    const categoryValue = e.target.value;
+    setCategory(categoryValue);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+
+    try {
+      const response = await fetch("http://localhost:4000/items", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(itemData),
+      });
+
+      const newItem = await response.json();
+      handleAddedItem(newItem);
+    } catch (error) {
+      console.error("Error adding item:", error);
+    }
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <input type="text" name="name" value={name} onChange={handleName} />
       </label>
 
       <label>
         Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
+        <select name="category" value={category} onChange={handleCategorySelect}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
